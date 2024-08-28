eccc0c52a9c1627d28d2dc112ac0d110d9c55af2
MFBarker <98473836+MFBarker@users.noreply.github.com>
2024-08-27 18:22:04 -0600

    New Test Project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region aa
+        #endregion
+        [TestMethod]
+        public void ValidLink()
+        {
+        }
+
+        [TestMethod]
+        public void NotValidLink()
+        { 
+            
+        }
+    }
+}
